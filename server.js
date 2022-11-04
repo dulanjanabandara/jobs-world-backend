8449932fd4c765c2ac49e108f03ad28cc7f552da
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-04 22:21:50 +0530

    Initial backend setup completed.

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
