6bb45aaf2ecb854d426f34c4e505cbc01a6c4a4d
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-05 00:26:50 +0530

    Auth routes created.

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+// db and authenticate user
 import connectDB from "./db/connect.js";
+
+// router
+import authRouter from "./routes/authRoutes.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+app.use("/api/v1/auth", authRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -22,7 +31,9 @@ const start = async () => {
       "<password>",
       process.env.DATABASE_PASSWORD
     );
+
     await connectDB(DB);
+
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
