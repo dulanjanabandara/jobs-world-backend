5946bdeb23225908d1b8255feae7133b0612f1da
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-05 14:30:12 +0530

    Advanced error handling mechanism was implemented using seperate classes for errors.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,18 @@
 import { StatusCodes } from "http-status-codes";
+import { NotFoundError, BadRequestError } from "../errors/index.js";
 import User from "../models/User.js";
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
+  const userAlreadyExists = User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
