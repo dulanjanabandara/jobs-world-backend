b8b4c3c04aa84a55fb357ce7009435080491d23f
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-09 01:23:38 +0530

    Delete jobs controller was implemented.

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -45,7 +45,15 @@ const updateJob = async (req, res, next) => {
 };
 
 const deleteJob = async (req, res, next) => {
-  res.send("delete a job");
+  const { id: jobId } = req.params;
+  const job = await Job.findOne({ _id: jobId });
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+  await job.remove();
+  res.status(StatusCodes.OK).json({ msg: "Success! Job removed" });
 };
 
 const showStats = async (req, res, next) => {
