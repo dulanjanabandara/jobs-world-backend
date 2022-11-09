fecfcde9152f5e41d436b98caad7a18cd3c86d54
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-10 04:33:35 +0530

    Modified the server file to serve static assets of the front end build.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
-const app = express();
 import dotenv from "dotenv";
-dotenv.config();
 import "express-async-errors";
 import morgan from "morgan";
+
+const app = express();
+dotenv.config();
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // db and authenticate user
 import connectDB from "./db/connect.js";
@@ -20,16 +25,18 @@ import authenticateUser from "./middleware/auth.js";
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome!" });
-});
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "API!" });
-});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
